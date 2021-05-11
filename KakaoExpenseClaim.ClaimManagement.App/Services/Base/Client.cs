505634fee5f9d47d874198fecce558391cb56387
min8grace <56316476+min8grace@users.noreply.github.com>
2021-05-10 22:40:40 -0700

    adding an App project, service client by the NSwaggerStudio

---
+++ KakaoExpenseClaim.ClaimManagement.App/Services/Base/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+
+namespace KakaoExpenseClaim.ClaimManagement.App.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+    }
+}
---
+++ KakaoExpenseClaim.ClaimManagement.App/Services/Base/IClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+
+namespace KakaoExpenseClaim.ClaimManagement.App.Services.Base
+{
+    public partial interface IClient
+    {
+        public HttpClient HttpClient { get; }
+
+    }
+}
